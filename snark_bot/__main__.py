79e5793eb166fd221f2b4dc357cb2d7e84a9427a
Bram Vereertbrugghen <bram@adimian.com>
2019-05-14 21:09:06 +0200

    Working version now, reading comments from a file

--- snark_bot/__main__.py
+++ snark_bot/__main__.py
@@ -1,15 +1,21 @@
+from slack import RTMClient
 from smart_getenv import getenv
 
-from snark_bot.bot_two import SlackBot
+from snark_bot.bot import SlackBot
+
+
+name = getenv("SLACK_BOT_NAME", type=str)
+token = getenv("SLACK_BOT_TOKEN", type=str)
+pattern = getenv("SLACK_BOT_RESPONSE_PATTERN", type=str)
+remarks_location = getenv("SNARKY_REMARKS_LOCATION", type=str)
+
+bot = SlackBot(name, pattern, remarks_location, token=token)
+
+
+@RTMClient.run_on(event="message")
+def _message_trigger(**kwargs):
+    bot.process_message(**kwargs)
 
 
 if __name__ == "__main__":
-    print("In main")
-    name = getenv("SLACK_BOT_NAME", type=str)
-    token = getenv("SLACK_BOT_TOKEN", type=str)
-    pattern = getenv("SLACK_BOT_RESPONSE_PATTERN", type=str)
-
-    bot = SlackBot(name, pattern, token=token)
-    print("Created bot")
-
     bot.start()
--- snark_bot/bot.py
+++ snark_bot/bot.py
@@ -1,75 +1,45 @@
 import re
-import time
-from typing import Optional
+import random
 
-from slack import RTMClient
+from slack import RTMClient, WebClient
 
 
 class SlackBot(object):
-    def __init__(
-        self,
-        name: str,
-        regex: str,
-        delay: int = 1,
-        command: Optional[str] = None,
-        **kwargs,
-    ):
+    def __init__(self, name: str, pattern: str, remarks_location: str, **kwargs):
         self.name = name
-        self.regex = regex
-
-        self.delay = delay
-        self.command = command or "do"
+        self.pattern = pattern
+        self.remarks_location = remarks_location
 
         self.slack_id = None
-        self.slack_client = RTMClient(**kwargs)
+        self._rtm_client = RTMClient(**kwargs)
+        self.slack_client = WebClient(**kwargs)
 
-        self._authenticate()
-
-    def _authenticate(self):
-        def _is_slack_config_okay() -> bool:
-            return self.slack_client.rtm_connect(with_team_state=False)
-
-        if _is_slack_config_okay():
-            self.slack_id = self.slack_client.api_call("auth.test")["user_id"]
-            print(f"Bot {self.name} is up and running.")
-        else:
-            print(f"Bot {self.name} connect connect to Slack.")
+        self.remarks = self.get_file_contents()
 
     def start(self):
-        while True:
-            messages = self.slack_client.rtm_read()
+        self._rtm_client.start()
 
-            command, channel = self.parse_commands(messages)
-            if command:
-                self.handle_command(command, channel)
+    def process_message(self, **kwargs):
+        data = kwargs.get("data")
+        text = data.get("text")
 
-            time.sleep(self.delay)
+        if text:
+            match = re.match(self.pattern, text, re.I)
+            if match is not None:
+                channel_id = data.get("channel")
+                thread_ts = data.get("ts")
 
-    def parse_commands(self, slack_events: list) -> tuple:
-        for event in slack_events:
-            if event["type"] == "message" and "subtype" not in event:
-                user_id, message = self.parse_direct_mention(event["text"])
-                if user_id == self.slack_id:
-                    return message, event["channel"]
+                self.slack_client.chat_postMessage(
+                    channel=channel_id,
+                    thread_ts=thread_ts,
+                    text=self.get_random_response(),
+                )
 
-        return None, None
+    def get_file_contents(self) -> list:
+        with open(self.remarks_location) as file:
+            return file.readlines()
 
-    def parse_direct_mention(self, message_text) -> tuple:
-        matches = re.search(self.regex, message_text)
+    def get_random_response(self) -> str:
+        remarks_length = len(self.remarks)
 
-        # First group == username, second group == message
-        if not matches:
-            return None, None
-
-        return (matches.group(1), matches.group(2).strip())
-
-    def handle_command(self, command, channel):
-        default_response = "Note sure what you mean."
-        response = None
-
-        if command.startswith("dink"):
-            response = "Sure man heyo"
-
-        self.slack_client.api_call(
-            "chat.postMessage", channel=channel, text=response or default_response
-        )
+        return self.remarks[random.randint(0, remarks_length - 1)]
